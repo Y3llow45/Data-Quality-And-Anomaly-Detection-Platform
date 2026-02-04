19815fbfc47576f40c0a1dec37a08a89ee554633
Y3llow45 <hristovborislav45@gmail.com>
2026-02-04 20:17:31 +0200

    Create User.java

---
+++ demo/src/main/java/com/example/demo/domain/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.demo.domain.entity;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+}

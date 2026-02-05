21624e5976424331e2fca252c2882a40bcffe693
Y3llow45 <hristovborislav45@gmail.com>
2026-02-05 21:33:01 +0200

    Create AuthService.java

---
+++ demo/src/main/java/com/example/demo/service/impl/AuthService.java
@@ -0,0 +1,57 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.dto.LoginRequestDTO;
+import com.example.demo.dto.LoginResponseDTO;
+import com.example.demo.dto.RegisterRequestDTO;
+import com.example.demo.dto.RegisterResponseDTO;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.domain.entity.User;
+import com.example.demo.security.jwtTokenProvider;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+public abstract class AuthService implements com.example.demo.service.AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final jwtTokenProvider jwtTokenProvider;
+
+    @Override
+    @Transactional
+    public RegisterResponseDTO register(RegisterRequestDTO request) {
+        if(userRepository.existsByEmail(request.email())) {
+            throw new RuntimeException("Email already in use");
+        }if(userRepository.existsByUsername(request.username())) {
+            throw new RuntimeException("Username already in use");
+        }
+
+        User user = new User();
+        user.setEmail(request.email());
+        user.setUsername(request.username());
+        user.setPassword(passwordEncoder.encode(request.password()));
+        userRepository.save(user);
+
+        String token = jwtTokenProvider.generateToken(user.getUsername());
+
+        return new RegisterResponseDTO(LocalDateTime.now(), token);
+    }
+
+     public LoginResponseDTO login(LoginRequestDTO request) {
+        if(userRepository.existsByUsernameOrEmail(request.usernameOrEmail())){
+            User user = userRepository.findByUsername(request.usernameOrEmail())
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+
+            if(passwordEncoder.matches(request.password(), user.getPassword())) {
+                String token = jwtTokenProvider.generateToken(user.getUsername());
+                return new LoginResponseDTO(token);
+            } else {
+                throw new RuntimeException("Invalid password");
+            }
+        } else {
+            throw new RuntimeException("User not found");
+        }
+    }
+}
